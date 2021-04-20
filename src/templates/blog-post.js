9e3c2654974b2695eec0e20d1483c2c3c268d668
Ubaid-Ali <syedubaidali2020@gmail.com>
2021-04-21 04:21:14 +0500

    Create blog-post.js

---
+++ src/templates/blog-post.js
@@ -0,0 +1,54 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Img from "gatsby-image"
+import Seo from "../components/seo"
+
+// import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+
+export const query = graphql`
+query($slug: String!) {
+  contentfulBlogPost(slug: { eq: $slug }) {
+    title
+    publishDate(formatString: "Do MMMM, YYYY")
+    featuredImage {
+      fluid(maxWidth: 750) {
+        ...GatsbyContentfulFluid
+      }
+    }
+    body {
+      raw
+    }
+  }
+}
+`
+
+const BlogPost = props => {
+    // console.log(`my-data-is => `, documentToReactComponents(props.data.contentfulBlogPost.body.raw) )
+
+    return (
+        <Layout>
+            <Seo title={props.data.contentfulBlogPost.title} />
+            <Link to="/blog/">Visit the Blog Page</Link>
+            <div className="content">
+                <h1>{props.data.contentfulBlogPost.title}</h1>
+                <span className="meta">
+                    Posted on {props.data.contentfulBlogPost.publishDate}
+                </span>
+
+                {props.data.contentfulBlogPost.featuredImage && (
+                    <Img
+                        className="featured"
+                        fluid={props.data.contentfulBlogPost.featuredImage.fluid}
+                        alt={props.data.contentfulBlogPost.title}
+                    />
+                )}
+                {/* ({documentToReactComponents(props.data.contenetfulBlogPost.body)}) */}
+            </div>
+        </Layout>
+    )
+}
+
+export default BlogPost
