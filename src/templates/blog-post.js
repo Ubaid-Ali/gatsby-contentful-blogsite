e0ac43d89119875093a14314978b0f76b3f58f65
Ubaid-Ali <syedubaidali2020@gmail.com>
2021-04-21 23:47:29 +0500

    Revert "displayed Rich Text File"
    This reverts commit 2b3fe0d1c0c5d2f627d674ac45db0f79e4456e84.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,50 +5,50 @@ import Layout from "../components/layout"
 import Img from "gatsby-image"
 import Seo from "../components/seo"
 
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+// import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
 
 export const query = graphql`
-  query($slug: String!) {
-    contentfulBlogPost(slug: { eq: $slug }) {
-      title
-      publishDate(formatString: "Do MMMM, YYYY")
-      featuredImage {
-        fluid(maxWidth: 750) {
-          ...GatsbyContentfulFluid
-        }
-      }
-      body {
-        raw
+query($slug: String!) {
+  contentfulBlogPost(slug: { eq: $slug }) {
+    title
+    publishDate(formatString: "Do MMMM, YYYY")
+    featuredImage {
+      fluid(maxWidth: 750) {
+        ...GatsbyContentfulFluid
       }
     }
+    body {
+      raw
+    }
   }
+}
 `
 
 const BlogPost = props => {
-  
-  const richTextData = JSON.parse(props.data.contentfulBlogPost.body.raw)
+    // console.log(`my-data-is => `, documentToReactComponents(props.data.contentfulBlogPost.body.raw) )
 
-  return (
-    <Layout>
-      <Seo title={props.data.contentfulBlogPost.title} />
-      <Link to="/blog/">Visit the Blog Page</Link>
-      <div className="content">
-        <h1>{props.data.contentfulBlogPost.title}</h1>
-        <span className="meta">
-          Posted on {props.data.contentfulBlogPost.publishDate}
-        </span>
+    return (
+        <Layout>
+            <Seo title={props.data.contentfulBlogPost.title} />
+            <Link to="/blog/">Visit the Blog Page</Link>
+            <div className="content">
+                <h1>{props.data.contentfulBlogPost.title}</h1>
+                <span className="meta">
+                    Posted on {props.data.contentfulBlogPost.publishDate}
+                </span>
 
-        {props.data.contentfulBlogPost.featuredImage && (
-          <Img
-            className="featured"
-            fluid={props.data.contentfulBlogPost.featuredImage.fluid}
-            alt={props.data.contentfulBlogPost.title}
-          />
-        )}
-        {documentToReactComponents(richTextData)}
-      </div>
-    </Layout>
-  )
+                {props.data.contentfulBlogPost.featuredImage && (
+                    <Img
+                        className="featured"
+                        fluid={props.data.contentfulBlogPost.featuredImage.fluid}
+                        alt={props.data.contentfulBlogPost.title}
+                    />
+                )}
+                {/* ({documentToReactComponents(props.data.contenetfulBlogPost.body)}) */}
+            </div>
+        </Layout>
+    )
 }
 
 export default BlogPost
