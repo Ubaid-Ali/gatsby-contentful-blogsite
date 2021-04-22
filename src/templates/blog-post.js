8fcd4621c1b3639fc41883298708b66072e91607
Ubaid-Ali <syedubaidali2020@gmail.com>
2021-04-23 04:14:17 +0500

    render image of contentful

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import Img from "gatsby-image"
 import Seo from "../components/seo"
 
+import { BLOCKS } from "@contentful/rich-text-types"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 export const query = graphql`
@@ -19,44 +20,51 @@ export const query = graphql`
       }
       body {
         raw
+        references {
+          contentful_id
+          title
+          fluid(maxWidth: 750) {
+            src
+          }
+        }
       }
     }
   }
 `
 
-const BlogPost = props => {
-  
-  const richTextBody = JSON.parse(props.data.contentfulBlogPost.body.raw)
-  console.log(`richTextBody =>`, richTextBody.content)
+const BlogPost = ({ data }) => {
+  // Richtext Contentful
+  let { body } = data?.contentfulBlogPost
 
+  const assets = new Map(body.references.map(ref => [ref.contentful_id, ref]))
   const options = {
     renderNode: {
-      "embedded-asset-block": node => {
-        const alt = ""
-      }
-    }
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        const url = assets.get(node.data.target.sys.id).fluid.src
+        const alt = assets.get(node.data.target.sys.id).title
+        return <img alt={alt} src={url} />
+      },
+    },
   }
-
-
 
   return (
     <Layout>
-      <Seo title={props.data.contentfulBlogPost.title} />
+      <Seo title={data.contentfulBlogPost.title} />
       <Link to="/blog/">Visit the Blog Page</Link>
       <div className="content">
-        <h1>{props.data.contentfulBlogPost.title}</h1>
+        <h1>{data.contentfulBlogPost.title}</h1>
         <span className="meta">
-          Posted on {props.data.contentfulBlogPost.publishDate}
+          Posted on {data.contentfulBlogPost.publishDate}
         </span>
 
-        {props.data.contentfulBlogPost.featuredImage && (
+        {data.contentfulBlogPost.featuredImage && (
           <Img
             className="featured"
-            fluid={props.data.contentfulBlogPost.featuredImage.fluid}
-            alt={props.data.contentfulBlogPost.title}
+            fluid={data.contentfulBlogPost.featuredImage.fluid}
+            alt={data.contentfulBlogPost.featuredImage.title}
           />
         )}
-        {documentToReactComponents(richTextBody)}
+        {documentToReactComponents(JSON.parse(body.raw), options)}
       </div>
     </Layout>
   )
