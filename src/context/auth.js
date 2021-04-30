8388f5b841cbb26ead6c205dcffdc5100fd88e90
Ubaid-Ali <syedubaidali2020@gmail.com>
2021-05-01 02:43:13 +0500

    change import method of firebase in auth.js

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,13 +1,13 @@
 import React, { createContext, useEffect, useState } from "react"
 import firebase from "gatsby-plugin-firebase"
-require('firebase/auth')
+
 export const AuthContext = createContext()
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState()
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged(user => setUser(user))
+    firebase.auth().onAuthStateChanged(usr => setUser(usr))
   }, [])
 
   return (
